d380576887d09dab5fd505fbcd053354fb5eff57
Justin Ball <justinball@gmail.com>
2010-11-13 21:18:00 -0700

    fixed helper bug

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,7 +2,7 @@ module ProjectsHelper
   
   def next_project_broadcast(project)
     if broadcast = project.next_broadcast
-      %Q{<span class="next-broadcast"><strong>Next Broadcast:</strong>#{broadcast.by_start.to_s(:date_and_time)}</span>}
+      %Q{<span class="next-broadcast"><strong>Next Broadcast:</strong>#{broadcast.start_at.to_s(:date_and_time)}</span>}
     end
   end
 
