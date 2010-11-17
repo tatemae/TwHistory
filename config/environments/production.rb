501c7b7227f37c5076c57dafca6a1a33f23e8f5a
Justin Ball <justinball@gmail.com>
2010-11-16 22:26:08 -0700

    removed solr config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,8 +47,4 @@ Twhistory::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.after_initialize do 
-    Sunspot.config.solr.url = 'http://localhost:9818/solr'
-  end
-  
 end
