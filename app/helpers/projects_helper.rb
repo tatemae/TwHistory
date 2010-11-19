6b1d26eb404d27dede8d509c658ee16356e6ae92
Justin Ball <justinball@gmail.com>
2010-11-19 00:17:39 -0700

    more styling fixes

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,17 +2,15 @@ module ProjectsHelper
   
   def next_project_broadcast(project)
     if broadcast = project.next_broadcast
-      %Q{<span class="next-broadcast"><a href="#{broadcast_path(broadcast)}"><strong>Next Broadcast:</strong>#{broadcast.start_at.to_s(:date_and_time)}</a>#{follow_link(project)}</span>}
+      %Q{<span class="next-broadcast"><a href="#{broadcast_path(broadcast)}"><strong>Next Broadcast:</strong>#{broadcast.start_at.to_s(:date_and_time)}</a>#{follow_link(project.next_broadcast)}</span>}
     else
       ''
     end
   end
 
-  def follow_link(project)
-    if broadcast = project.next_broadcast
-      if broadcast.authentication
-        return link_to(translate('general.follow'), "http://twitter.com/#{broadcast.authentication.nickname}", :class => 'follow', :target => 'blank')
-      end
+  def follow_link(broadcast)
+    if broadcast.authentication
+      return link_to(translate('general.follow'), "http://twitter.com/#{broadcast.authentication.nickname}", :class => 'follow', :target => 'blank')
     end
   end
   
