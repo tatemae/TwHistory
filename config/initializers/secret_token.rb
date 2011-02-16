6b669828c3bfc458b31810255b2646ec63c533ef
Justin Ball <justinball@gmail.com>
2011-02-15 18:15:18 -0700

    removed secure token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Twhistory::Application.config.secret_token = 'ba8453f7520bd37a48bb5dfcb29401bd5ba6dd4aab3a1180dbbfbc497cc611888a71caf7f39da7bf985f2c181eb87c384c88c61e70022864332da93431b6a4cd'
+Twhistory::Application.config.secret_token = Secrets.secret_token
