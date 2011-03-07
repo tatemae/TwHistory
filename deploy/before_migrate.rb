444ced9b41c1cc3e718582c66267df182eaece0f
Justin Ball <justinball@gmail.com>
2011-03-07 16:01:15 -0700

    added ey related deploy items

---
+++ deploy/before_migrate.rb
@@ -0,0 +1,5 @@
+run "ln -nfs #{shared_path}/config/secrets.yml #{release_path}/config/"
+run "ln -nfs #{shared_path}/config/newrelic.yml #{release_path}/config/"
+run "ln -nfs #{shared_path}/cache/ #{release_path}/tmp/"
+
+run "cd #{current_path} && RAILS_ENV=#{rails_env} script/delayed_job stop"
