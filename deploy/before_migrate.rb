38a506e2c70e1e5cb05f4befcc475be5dc882c9c
Justin Ball <justinball@gmail.com>
2011-03-09 09:57:31 -0700

    removed deploy items

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,5 +1,5 @@
 run "ln -nfs #{shared_path}/config/secrets.yml #{release_path}/config/"
 run "ln -nfs #{shared_path}/config/newrelic.yml #{release_path}/config/"
-run "ln -nfs #{shared_path}/cache/ #{release_path}/tmp/"
+#run "ln -nfs #{shared_path}/cache/ #{release_path}/tmp/"
 
-run "cd #{current_path} && RAILS_ENV=#{rails_env} script/delayed_job stop"
+#run "cd #{current_path} && RAILS_ENV=#{rails_env} script/delayed_job stop"
